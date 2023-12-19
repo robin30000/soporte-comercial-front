22d9d73aae37a119c4d05e96d82fe3f24c1adf7a
Robin <racastro218@gmail.com>
2023-12-19 10:13:59 -0500

    correccion perfiles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,68 +11,82 @@ export interface Usuario {
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm: FormGroup;
   user1: any;
   loding = false;
 
   @ViewChild('lform') loginFormDirective: any;
 
-
   formErrors = {
     username: '',
-    password: ''
-  }
+    password: '',
+  };
 
   validationMsj = {
-    'username': {
-      'required': 'El nombre de usuario es obligatorio'
+    username: {
+      required: 'El nombre de usuario es obligatorio',
     },
-    'password': {
-      'required': 'Debe escribir la contraseña'
-    }
-  }
+    password: {
+      required: 'Debe escribir la contraseña',
+    },
+  };
 
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private fb: FormBuilder,
     private alerts: MatSnackBar,
-    private authService: AuthService) {
+    private authService: AuthService
+  ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      password: ['', Validators.required],
+    });
   }
 
   public iniciarSesion() {
     let login = {
-      'username': this.loginForm.value.username,
-      'password': this.loginForm.value.password
-    }
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password,
+    };
 
-    this.authService.login(login).subscribe(res => {
+    this.authService.login(login).subscribe((res) => {
       if (res.state) {
         let perfil = res.data.Perfil;
         localStorage.setItem('perfil', perfil);
         localStorage.setItem('user', this.loginForm.value.username);
         localStorage.setItem('menu', JSON.stringify(res.data.menu));
+console.log(perfil);
 
-        this.router.navigate(['ConsultaPedido']);
-      }else{
+        switch (perfil) {
+          case '2':
+            this.router.navigate(['ventas-instale-tiendas']);
+            break;
+          case '3':
+            this.router.navigate(['Gescom']);
+            break;
+          case '4':
+            this.router.navigate(['ConsultaEquiposInstalados']);
+            break;
+          case '5':
+            this.router.navigate(['ConsultaPedido']);
+            break;
+          default:
+            this.router.navigate(['ConsultaEquiposInstalados']);
+        }
+
+      } else {
         Swal.fire({
           icon: 'error',
           title: 'Oops...',
           text: res.msj,
-          timer: 4000
-        })
+          timer: 4000,
+        });
       }
-    })
+    });
   }
 
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
